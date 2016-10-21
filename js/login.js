f65910bd38e2eb560118831fcdd4e0dc9b667fbd
wbc20160871 <294916011@qq.com>
2016-10-21 11:03:01 +0800

    修改js html文件

--- js/login.js
+++ js/login.js
@@ -1,1 +1,13 @@
-alert('99999wewds');
+//alert('99999wewds');
+
+/*var function() {
+    var i = 0;
+
+    return function() {
+        console.log(i++)
+    }
+}*/
+
+
+/* var a = (window.foo || (window.foo = "bar"));
+ console.log
